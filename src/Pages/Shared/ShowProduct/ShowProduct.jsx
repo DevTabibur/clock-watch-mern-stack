0062fa4e6795bbd9642b0d0e512f9fe223d9f5e8
DevTabibur <tobiburrohman2@gmail.com>
2022-05-12 20:02:47 +0600

    modal is working

--- src/Pages/Shared/ShowProduct/ShowProduct.jsx
+++ src/Pages/Shared/ShowProduct/ShowProduct.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import './ShowProduct.css'
-import {Col} from 'react-bootstrap'
+import React from "react";
+import "./ShowProduct.css";
+import { Col } from "react-bootstrap";
+import ShowModal from "../Modal/ShowModal";
 
-const ShowProduct = ({pd}) => {
+const ShowProduct = ({ pd }) => {
+  const [modalShow, setModalShow] = React.useState(false);
 
   return (
     <>
-        <Col md={3} lg={3} sm={6}>
+      <Col md={3} lg={3} sm={6}>
         <div className="product-box">
-        <img src={pd.image} alt="product__img" />
+          <img src={pd.image} alt="product__img" />
 
-        <div className="hover-img-div">
-        <button>Add to Cart</button>
-        <button>Quick Overview</button>
+          <div className="hover-img-div">
+            <button>Add to Cart</button>
+            <button onClick={() => setModalShow(true)}>Quick Overview</button>
+            <ShowModal show={modalShow} onHide={() => setModalShow(false)} />
+          </div>
+
+          <div className="info">
+            <h3 className="product-name">{pd.name}</h3>
+            <h3 className="product-price">{pd.price}</h3>
+          </div>
         </div>
-
-        <div className="info">
-        <h3 className='product-name'>{pd.name}</h3>
-        <h3 className='product-price'>{pd.price}</h3>
-        </div>
-
-        </div>
-
-        </Col>
+      </Col>
     </>
-  )
-}
+  );
+};
 
-export default ShowProduct
+export default ShowProduct;
